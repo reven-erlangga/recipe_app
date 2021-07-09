c5e2d3906b38946e3d7d421a2f869b0a0613e3e2
reven erlangga <reven.erlangga@gmail.com>
2021-07-10 05:39:24 +0700

    membuat recipe api

--- lib/repositories/recipe_api.dart
+++ lib/repositories/recipe_api.dart
@@ -1,1 +1,26 @@
+import 'dart:convert';
+import 'package:recipe_app/models/recipe.dart';
+import 'package:http/http.dart' as http;
 
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https("https://yummly2.p.rapidapi.com", "/feeds/list",
+        {"start": "0", "limit": "18", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "9310a017bamsh98468c57281d6c9p1b1467jsn20fccef2a038",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
